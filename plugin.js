2253c31013b10b18f138e09d220c5a89ef73aebe
arnaudm <arnaud.mary@gmail.com>
2017-05-24 11:49:34 +0200

    Minor refactor

--- plugin.js
+++ plugin.js
@@ -1,33 +1,39 @@
 
 'use strict';
 
-//
-class CmsIgoPlugin {
+const _       = require('lodash');
 
-  constructor() {
-    this.options = {
-      adminpath:  '/admin'
-    };
-    this.dirname = __dirname;
-  }
 
-  get routes() {
-    return {
-      main:   require('./src/routes/main'),
-      admin:  require('./src/routes/admin'),
-    };
-  }
-
-  loadMenu(menu_id) {
-    const CMS = require('./src/services/CMS');
-    return function(req, res, next) {
-      CMS.loadMenu(menu_id, function(err, menu) {
-        res.locals['menu_' + menu_id] = menu;
-        next();
-      });
-    };
-  }
-
+const plugin = {
+  options: {
+    adminpath:  '/admin'
+  },
+  dirname: __dirname
 };
 
-module.exports = new CmsIgoPlugin();
+// init
+plugin.init = function(igo) {
+  plugin.igo = igo;
+  plugin.routes = {
+    main:   require('./src/routes/main'),
+    admin:  require('./src/routes/admin'),
+  };
+};
+
+//
+plugin.configure = function(options) {
+  _.merge(plugin.options, options);
+};
+
+//
+plugin.loadMenu = function(menu_id) {
+  const CMS = require('./src/services/CMS');
+  return function(req, res, next) {
+    CMS.loadMenu(menu_id, function(err, menu) {
+      res.locals['menu_' + menu_id] = menu;
+      next();
+    });
+  };
+};
+
+module.exports = plugin;
