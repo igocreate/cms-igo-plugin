53d50d223501a458a72eee55efd672fee368a84f
Benoit <benoit.sourd@gmail.com>
2018-03-21 17:42:42 +0100

    Add category slug to pages

--- src/models/Page.js
+++ src/models/Page.js
@@ -25,6 +25,7 @@ const schema = {
     'excerpt',
     'body',
     'category',
+    'category_slug',
     'tags',
     'image_id',
     'menu_id',
@@ -54,23 +55,34 @@ const schema = {
 
 class Page extends Model(schema) {
 
+  constructor(data) {
+    super(data);
+    if (this.category && !this.category_slug) {
+      this.category_slug = _.kebabCase(this.category);
+    }
+  }
+
   beforeCreate(callback) {
-    this.slug   = StringUtils.slugify(this.slug || this.title) || null;
+    this.slug          = StringUtils.slugify(this.slug || this.title) || null;
+    if (this.category) {
+      this.category_slug = StringUtils.slugify(this.category);
+    }
     if (!this.published_at && this.status === 'published') {
       this.published_at = new Date();
     }
     callback();
   }
-  //
-  // beforeUpdate(values, callback) {
-  //   if (!this.slug) {
-  //     values.slug = StringUtils.slugify(values.slug || values.title || this.title) || null;
-  //   }
-  //   if (!this.published_at && this.status === 'published') {
-  //     values.published_at = new Date();
-  //   }
-  //   callback();
-  // }
+
+  beforeUpdate(values, callback) {
+    values.category_slug = values.category ? StringUtils.slugify(values.category) : null;
+    // if (!this.slug) {
+    //   values.slug = StringUtils.slugify(values.slug || values.title || this.title) || null;
+    // }
+    // if (!this.published_at && this.status === 'published') {
+    //   values.published_at = new Date();
+    // }
+    callback();
+  }
 
   get url() {
     return [this.slug, this.id].join('-');
